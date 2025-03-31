277cf826caa38d898ba988e3195e3469e8488f32
MarceloKobayashi <marcelohk2004@gmail.com>
2025-03-31 18:11:56 -0300

    0331 - LoginCadastro feitos

--- js/loginCadastro.js
+++ js/loginCadastro.js
@@ -17,6 +17,10 @@ document.getElementById('cadastroForm').addEventListener('submit', async (event)
     const email = document.getElementById('cadastro-email').value;
     const senha = document.getElementById('cadastro-senha').value;
     const telefone = document.getElementById('cadastro-telefone').value;
+    const tipo = document.getElementById('cadastro-tipo').value;
+
+    console.log("Dados enviados:", { nome, email, senha, telefone, tipo });
+    console.log("JSON enviado:", JSON.stringify({ nome, email, senha, telefone, tipo }));
 
     try {
         const response = await fetch(`${API_URL}/cadastrar`, {
@@ -28,7 +32,7 @@ document.getElementById('cadastroForm').addEventListener('submit', async (event)
                 nome,
                 email,
                 senha,
-                tipo: "usuario",
+                tipo,
                 telefone
             })
         });
@@ -36,11 +40,48 @@ document.getElementById('cadastroForm').addEventListener('submit', async (event)
         const data = await response.json();
         if (response.ok) {
             alert('Cadastro realizado com sucesso!');
+            
+            document.getElementById('div-cadastro').style.display = 'none';
+            document.getElementById('div-login').style.display = 'block';
         } else {
             alert(data.detail || 'Erro ao realizar o cadastro');
         }
     } catch (error) {
         alert('Erro ao se comunicar com o servidor');
+        console.error(error);
+    }
+})
+
+document.getElementById('loginForm').addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("login-email").value;
+    const senha = document.getElementById("login-senha").value;
+    
+    console.log("Dados enviados:", { email, senha});
+    console.log("JSON enviado:", JSON.stringify({ email, senha }));
+
+    try {
+        const response = await fetch(`${API_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, senha })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert('Login realizado com sucesso.');
+            localStorage.setItem('token', data.access_token);
+
+            //window.location.href = 'home.html'
+        } else {
+            alert(data.detail || 'Erro ao fazer login.');
+        }
+    } catch (error) {
+        alert('Erro ao se comunicar com o servidor.');
         console.error(error);
     }
 })
